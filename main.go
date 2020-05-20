ae552f9e06d43b6eddfc164f9cdd2129b840d819
momose tomohiro <momose-tomohiro@gb-sense.com>
2020-05-20 21:57:36 +0900

    DB登録処理を修正
    画面遷移なしで登録されるように修正

--- main.go
+++ main.go
@@ -2,56 +2,70 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type todoList struct {
+	Schedule  string `json:"schedule"`
+	Priority  string `json:"priority"`
+	TimeLimit string `json:"timeLimit"`
+}
+
 func main() {
 	db, err := sql.Open("mysql", "root:tomoaki7@/todo")
 	if err != nil {
 		fmt.Println(err.Error())
+		return
 	}
 	defer db.Close()
-
-	var version string
-	db.QueryRow("SELECT VERSION()").Scan(&version)
-	fmt.Println("Connected to: ", version)
 
 	http.Handle("/", http.FileServer(http.Dir("views")))
 	http.HandleFunc("/register", register)
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
 func register(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+	var form todoList
 
-	schedule := r.Form["schedule"][0]
-	priority := r.Form["priority"][0]
-	timeLimit := r.Form["time_limit"][0]
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+	}
+	fmt.Println(string(b))
+	json.Unmarshal(b, &form)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+	}
+
+	schedule := form.Schedule
+	priority := form.Priority
+	timeLimit := form.TimeLimit
 
 	db, err := sql.Open("mysql", "root:tomoaki7@/todo")
 	if err != nil {
-		fmt.Println(err.Error())
+		return
 	}
 	defer db.Close()
 	stmt, err := db.Prepare("INSERT INTO trn_todo(schedule, priority, time_limit) VALUES(?,?,?)")
 	if err != nil {
-		panic(err.Error())
+		http.Error(w, err.Error(), 400)
 	}
 	defer stmt.Close()
 
 	result, err := stmt.Exec(schedule, priority, timeLimit)
 	if err != nil {
-		panic(err.Error())
+		http.Error(w, err.Error(), 400)
 	}
 
 	id, err := result.LastInsertId()
 	if err != nil {
-		panic(err.Error())
+		http.Error(w, err.Error(), 400)
 	}
 	fmt.Println(id)
 
-	http.Redirect(w, r, "/", 303)
 }
