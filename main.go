2f4b0315ec9aac775adad8618c6f39c31e63516d
momose tomohiro <momose-tomohiro@gb-sense.com>
2020-05-25 14:58:23 +0900

    手本を元にし各種処理を修正しました

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 type todo struct {
-	ID        string `db:"id" json:"id"`
+	ID        int    `db:"id" json:"id"`
 	Schedule  string `db:"schedule" json:"schedule"`
 	Priority  string `db:"priority" json:"priority"`
 	TimeLimit string `db:"time_limit" json:"timeLimit"`
@@ -19,113 +19,93 @@ type todo struct {
 
 type todoList []todo
 
-var dbConn *sql.DB
-
-func connect() (db *sql.DB, err error) {
-	dbConn, err := sql.Open("mysql", "root:0111@/todo")
-	if err != nil {
-		log.Println(err)
-	}
-	return dbConn, err
-}
+var db *sql.DB
 
 func main() {
-	db, err := connect()
+	var err error
+	db, err = sql.Open("mysql", "root:tomoaki7@/todo")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	dbConn = db
+	defer db.Close()
+
 	http.Handle("/", http.FileServer(http.Dir("views")))
-	http.HandleFunc("/register", register)
-	http.HandleFunc("/display", display)
-	http.HandleFunc("/remove", remove)
+	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			display(w, r)
+		case http.MethodPost:
+			register(w, r)
+		case http.MethodDelete:
+			remove(w, r)
+		}
+	})
+	log.Println("start http server :8888")
+
+	/*
+		http.HandleFunc("/register", register)
+		http.HandleFunc("/display", display)
+		http.HandleFunc("/remove", remove)
+	*/
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
 func register(w http.ResponseWriter, r *http.Request) {
-	form := todo{}
+	var form todo
 
 	if err := json.NewDecoder(r.Body).Decode(&form); err != nil {
-		http.Error(w, err.Error(), 400)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	schedule := form.Schedule
-	priority := form.Priority
-	timeLimit := form.TimeLimit
-
-	stmt, err := dbConn.Prepare("INSERT INTO trn_todo(schedule, priority, time_limit) VALUES(?,?,?)")
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(schedule, priority, timeLimit)
-	if err != nil {
-		http.Error(w, err.Error(), 400)
+	if _, err := db.Exec("INSERT INTO trn_todo (schedule, priority, time_limit) values(?, ?, ?)", form.Schedule, form.Priority, form.TimeLimit); err != nil {
+		http.Error(w, err.Error(), 500)
 		return
 	}
 }
 func display(w http.ResponseWriter, r *http.Request) {
-	todoList := todoList{}
-	rows, err := dbConn.Query("SELECT id, schedule, priority, time_limit FROM trn_todo")
+	var todoList todoList
+	rows, err := db.Query("SELECT id, schedule, priority, time_limit FROM trn_todo")
 	if err != nil {
-		http.Error(w, err.Error(), 400)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
 	for rows.Next() {
-		todo := todo{}
-		err := rows.Scan(&todo.ID, &todo.Schedule, &todo.Priority, &todo.TimeLimit)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
+		var (
+			id        int
+			schedule  string
+			priority  string
+			timeLimit string
+		)
+		if err := rows.Scan(&id, &schedule, &priority, &timeLimit); err != nil {
+			http.Error(w, err.Error(), 500)
 			return
 		}
-		todoList = append(todoList, todo)
+		todoList = append(todoList, todo{id, schedule, priority, timeLimit})
 	}
-	defer rows.Close()
-
-	JSONTodoList, err := json.Marshal(todoList)
-	if err != nil {
-		http.Error(w, err.Error(), 400)
+	if err := json.NewEncoder(w).Encode(&todoList); err != nil {
+		http.Error(w, err.Error(), 500)
 		return
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(JSONTodoList)
 }
 func remove(w http.ResponseWriter, r *http.Request) {
-	form := todo{}
+	id := r.URL.Query().Get("id")
 	//元の処理
 	/*
 		b, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			http.Error(w, err.Error(), 400)
+			http.Error(w, err.Error(), 500)
 			return
 		}
 		json.Unmarshal(b, &form)
 		if err != nil {
-			http.Error(w, err.Error(), 400)
+			http.Error(w, err.Error(), 500)
 			return
 		}
 	*/
-	if err := json.NewDecoder(r.Body).Decode(&form); err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-
-	targetID := form.ID
-
-	stmt, err := dbConn.Prepare("DELETE FROM trn_todo WHERE id = ?")
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec(targetID)
-	if err != nil {
-		http.Error(w, err.Error(), 400)
+	if _, err := db.Exec("DELETE FROM trn_todo WHERE id = ?", id); err != nil {
+		http.Error(w, err.Error(), 500)
 		return
 	}
 }
--- views/index.js
+++ views/index.js
@@ -1,73 +1,98 @@
 window.onload = getTodoList();
+const table = document.getElementById(`todo_list`);
 
 const registerButton = document.getElementById(`register`);
 registerButton.onclick = function() {
-  const form = {};
-  form.schedule = document.schedule_data.schedule.value;
-  form.priority = document.schedule_data.priority.value;
-  form.timeLimit = document.schedule_data.time_limit.value;
-  const JsonData = JSON.stringify(form);
-  fetch("/register", {
-    method: "post",
-    body: JsonData
+  const form = {
+    schedule : document.schedule_data.schedule.value,
+    priority : document.schedule_data.priority.value,
+    timeLimit : document.schedule_data.time_limit.value
+  };
+  
+  fetch('/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(form)
   }).then(response => {
-    if (response.status === 200) {
-      alert("登録に成功しました");
+    if (response.ok) {
+      alert('登録に成功しました');
+      document.schedule_data.schedule.value = "";
+      document.schedule_data.time_limit.value = "";
       getTodoList();
     }
-  });
+  }).catch((err) => {
+    console.log(err);
+  })
 };
 
 function getTodoList() {
-  fetch("/display")
+  fetch('/todos')
     .then(response => {
-      if (response.status === 200) {
+      if (response.ok) {
         return response.json();
       }
     })
     .then(todoList => {
-      const table = document.getElementById(`todo_list`);
-      while (table.rows[0]) {
-        table.deleteRow(0);
+      table.innerHTML = ""
+      for(todo of todoList){
+        const tr = document.createElement("tr")
+        const schedule = document.createElement("td")
+        schedule.textContent = todo.schedule
+        tr.appendChild(schedule)
+        const priority = document.createElement("td")
+        priority.textContent = todo.priority
+        tr.appendChild(priority)
+        const timeLimit = document.createElement("td")
+        timeLimit.textContent = todo.timeLimit
+        tr.appendChild(timeLimit)
+        const button = document.createElement('button');
+        button.type = 'button';
+        button.value = todo.id;
+        button.textContent = '削除';
+        button.addEventListener('click', removeSchedule, false);
+        tr.appendChild(button)
+
+        table.appendChild(tr)
       }
+      /*以前
       todoList.forEach(function(element) {
-        let schedule = element.schedule;
-        let priority = element.priority;
-        let timeLimit = element.timeLimit;
-        let id = element.id;
         let row = table.insertRow(-1);
         let cell = row.insertCell(-1);
-        let text = document.createTextNode(schedule);
+        let text = document.createTextNode(element.schedule);
         cell.appendChild(text);
         cell = row.insertCell(-1);
-        text = document.createTextNode(priority);
+        text = document.createTextNode(element.priority);
         cell.appendChild(text);
         cell = row.insertCell(-1);
-        text = document.createTextNode(timeLimit);
+        text = document.createTextNode(element.timeLimit);
         cell.appendChild(text);
         cell = row.insertCell(-1);
-        let button = document.createElement("button");
-        button.type = "button";
-        button.value = id;
-        button.innerText = "削除";
+        let button = document.createElement('button');
+        button.type = 'button';
+        button.value = element.id;
+        button.innerText = '削除';
 
         cell.appendChild(button);
-        button.addEventListener("click", removeSchedule, false);
+        button.addEventListener('click', removeSchedule, false);
       });
-    });
+      */
+    }).catch((err) => {
+      console.log(err);
+    })
 }
 
 function removeSchedule(event) {
-  const val = event.target.value;
-  const form = { id: val };
-  const JsonData = JSON.stringify(form);
-  fetch("/remove", {
-    method: "post",
-    body: JsonData
+  const id = event.target.value;
+  fetch(`/todos?id=${id}`, {
+    method: 'DELETE'
   }).then(response => {
-    if (response.status === 200) {
-      alert("削除しました");
+    if (response.ok) {
+      alert('削除しました');
       getTodoList();
     }
-  });
+  }).catch((err) => {
+    console.log(err);
+  })
 }
