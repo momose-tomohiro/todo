0685f310395fa563eaaab202f88fc60a31868fde
momose tomohiro <momose-tomohiro@gb-sense.com>
2020-05-26 17:32:49 +0900

    一覧表示をmethods:{}の内部へ移動、それに伴った登録、削除時の呼び出し変更

--- views/index.js
+++ views/index.js
@@ -11,16 +11,27 @@ const todos = new Vue({
       }).then(response => {
         if (response.ok) {
           alert('削除しました');
-          getTodoList();
+          this.display()
         }
       }).catch((err) => {
         console.log(err);
       })
-    }
+    },
+    display: onload = function() {
+        fetch('/todos')
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            }
+          })
+          .then(todoList => {
+            todos.todoList = todoList
+          }).catch((err) => {
+            console.log(err);
+          })
+      }
   }
 })
-
-getTodoList();
 
 const registerForm = new Vue({
   el:'#schedule_data',
@@ -43,7 +54,7 @@ const registerForm = new Vue({
           alert('登録に成功しました');
           this.schedule = "";
           this.time_limit = "";
-          getTodoList();
+          todos.display()
         }
       }).catch((err) => {
         console.log(err);
@@ -52,16 +63,3 @@ const registerForm = new Vue({
   }
 })
 
-function getTodoList() {
-  fetch('/todos')
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-    })
-    .then(todoList => {
-      todos.todoList = todoList
-    }).catch((err) => {
-      console.log(err);
-    })
-}
