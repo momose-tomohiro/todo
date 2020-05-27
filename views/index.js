91b18ea0d203ecd076c68082893f3b2b75c0bc24
momose tomohiro <momose-tomohiro@gb-sense.com>
2020-05-27 10:59:16 +0900

    v-modelの書き方が悪く、エラーが出ていたのを修正

--- views/index.js
+++ views/index.js
@@ -1,7 +1,14 @@
 const todos = new Vue({
   el:'#todoPage',
   data:{
-      todoList: []
+      todoList: [
+        { 
+          id:'',
+          schedule:'',
+          priority:'',
+          timeLimit:''
+        }
+      ]
   },
   methods:{
     display:onload=function(){
@@ -19,9 +26,9 @@ const todos = new Vue({
     },
     register:function(){
       const form = {
-        schedule : this.schedule,
-        priority : this.priority,
-        timeLimit : this.time_limit
+        schedule :  this.todoList.schedule,
+        priority : this.todoList.priority,
+        timeLimit : this.todoList.timeLimit
       };
       
       fetch('/todos', {
@@ -33,8 +40,8 @@ const todos = new Vue({
       }).then(response => {
         if (response.ok) {
           alert('登録に成功しました');
-          this.schedule = "";
-          this.time_limit = "";
+          this.todoList.schedule = "";
+          this.todoList.time_limit = "";
           this.display()
         }
       }).catch((err) => {
