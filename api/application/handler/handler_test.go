2f2e4911518fd25cad5f208d423fb797372af7fa
momose tomohiro <momose-tomohiro@gb-sense.com>
2020-06-16 16:09:28 +0900

    一覧取得処理のテストを実装

--- api/application/handler/handler_test.go
+++ api/application/handler/handler_test.go
@@ -1,29 +1,32 @@
 package handler
 
 import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
 	"strconv"
+	"strings"
 	"testing"
 	"todo/api/domain/model"
 
 	"github.com/labstack/echo"
 )
 
-type TodoService struct {
+type todoService struct {
 	err error
 }
 
-func (t *TodoService) GetTodoService() ([]model.Todo, error) {
+func (t *todoService) GetTodoService() ([]model.Todo, error) {
 	//t.err = errors.New("test error")
 	if t.err != nil {
 		return nil, t.err
 	}
 	return []model.Todo{
 		{ID: 1, Schedule: "test", Priority: "高", TimeLimit: "2020-06-12"},
-		{ID: 2, Schedule: "test2", Priority: "低", TimeLimit: "2020-06-12"},
 	}, nil
 }
 
-func (t *TodoService) RegisterTodoService(c echo.Context) error {
+func (t *todoService) RegisterTodoService(c echo.Context) error {
 	body := new(model.Todo)
 	if err := c.Bind(body); err != nil {
 		return err
@@ -31,7 +34,7 @@ func (t *TodoService) RegisterTodoService(c echo.Context) error {
 	return t.err
 }
 
-func (t *TodoService) RemoveTodoService(c echo.Context) error {
+func (t *todoService) RemoveTodoService(c echo.Context) error {
 	var body model.Todo
 	var err error
 
@@ -43,11 +46,59 @@ func (t *TodoService) RemoveTodoService(c echo.Context) error {
 }
 
 func TestGetTodoList(t *testing.T) {
+	serviceErr := errors.New("test error")
+	tests := []struct {
+		serviceErr error
+		endpoint   string
+		wantStatus int
+		wantBody   string
+	}{
+		{nil, "/todos", 200, `[{"id":1,"schedule":"test","priority":"高","time_limit":"2020-06-12"}]`},
+		{serviceErr, "/todos", 500, `{"message":"cannot get users: service layer error"}`},
+	}
+
+	var h TodoHandler
+	for i, tt := range tests {
+		if tt.serviceErr != nil {
+			h = NewTodoHandler(&todoService{serviceErr})
+		} else {
+			h = NewTodoHandler(&todoService{})
+		}
+
+		e := echo.New()
+
+		req := httptest.NewRequest(http.MethodGet, tt.endpoint, nil)
+		rec := httptest.NewRecorder()
+
+		c := e.NewContext(req, rec)
+
+		if err := h.GetTodoList(c); err != nil {
+			t.Fatalf("case:[%d] GET %s error:%s", i, tt.endpoint, err)
+		}
+
+		if rec.Code != tt.wantStatus {
+			t.Fatalf("case:[%d] GET %s unexpected status code, want=%d, got=%d", i, tt.endpoint, tt.wantStatus, rec.Code)
+		}
+		gotBody := strings.Trim(rec.Body.String(), "\n\r")
+		if gotBody != tt.wantBody {
+			t.Fatalf("case:[%d] GET %s unexpected body, want=%s, got=%s", i, tt.endpoint, tt.wantBody, gotBody)
+		}
+	}
 	t.Logf("OK")
 }
 
 /*
 func TestGetTodoList(t *testing.T) {
+	serviceErr := errors.New("test error")
+	tests := []struct {
+		serviceErr error
+		endpoint   string
+		wantStatus int
+		wantBody   string
+	}{
+		{nil, "/todos", 200, `[{"id":1,"Schedule":"test","Priority":"高","TimeLimit","2020-06-16"}]`},
+	}
+
 	testTodo := &TodoService{}
 	test, err := testTodo.GetTodoService()
 	if err != nil {
